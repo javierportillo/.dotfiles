95566bacc07ca0d53dc2f41df170f17bc0a42d4f
Javier Portillo <17260001+javierportillo@users.noreply.github.com>
2023-05-20 16:02:59 -0600

    add instalation of oh-my-tmux

--- setup/setup_on_zsh.sh
+++ setup/setup_on_zsh.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+echo "install oh-my-tmux"
+if [ ! -d "$HOME/.local/oh-my-tmux" ]; then
+	git clone https://github.com/gpakosz/.tmux.git ~/.local/oh-my-tmux
+fi
+
+ln -s ~/.local/oh-my-tmux/.tmux.conf ~/.config/tmux/tmux.conf
+
 echo "install asdf plugins"
 asdf plugin add nodejs https://github.com/asdf-vm/asdf-nodejs.git
 asdf plugin add erlang https://github.com/asdf-vm/asdf-erlang.git
