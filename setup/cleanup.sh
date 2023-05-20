c4805d9426c6ca70f004d850cb304286bf24563f
Javier Portillo <17260001+javierportillo@users.noreply.github.com>
2023-05-19 20:36:06 -0600

    changes in setup and cleanup scripts

--- setup/cleanup.sh
+++ setup/cleanup.sh
@@ -2,9 +2,13 @@
 
 OHMYZSH_DIR="$HOME/.oh-my-zsh"
 CUSTOM_DIR="${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}"
+DOTFILES_DIR="$(dirname "$(dirname "$(readlink -f "$0")")")"
 
 echo "change workind dir to home"
 cd $HOME
+
+echo "removing stowed files from $DOTFILES_DIR/home"
+stow -vv -t $HOME -d $DOTFILES_DIR -D home
 
 echo "Removing p10k"
 rm -rf $CUSTOM_DIR/themes/powerlevel10k
@@ -12,15 +16,12 @@ rm -rf .p10k.zsh
 
 echo "Removing oh-my-zsh"
 rm -rf $OHMYZSH_DIR
-rm .shell.pre-oh-my-zsh
+rm -f .shell.pre-oh-my-zsh
+
+echo "cleaning up files"
+rm -f .zcompdump*
+rm -f .zsh_history
 
 echo "Reverting to bash"
 chsh -s $(which bash)
-
-echo "Cleaning up zsh config (no need to uninstall)"
-rm .zsh*
-rm .zcompdump*
-
-echo "cleaning up .gitconfig"
-rm .gitconfig
 
--- setup/setup.sh
+++ setup/setup.sh
@@ -4,24 +4,26 @@ set -e
 OHMYZSH_DIR="$HOME/.oh-my-zsh"
 CUSTOM_DIR="${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}"
 
-echo "change workind dir to home"
-cd $HOME
-
-if ! command -v git &> /dev/null; then
-	echo "Git is not installed. Aborting."
-	exit 1
-fi
+DOTFILES_DIR="$(dirname "$(dirname "$(readlink -f "$0")")")"
 
 echo "Installing dependencies"
 
-sudo apt install zsh -y
+sudo apt install git curl zsh stow xdg-user-dirs exa lfm python3-pip
 
+echo "stowing files from $DOTFILES_DIR/home"
+stow -vv -t $HOME -d $DOTFILES_DIR -S home
+
+echo "updating xdg dirs"
+xdg-user-dirs-update
+
+echo "change workind dir to home"
+cd $HOME
 
 echo "install oh-my-zsh"
 
 if [ ! -d "$OHMYZSH_DIR" ]; then
 	read -p "remember to exit zsh and come back to bash to continue the configuration"
-	sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+	sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended --keep-zshrc
 else
 	echo "$OHMYZSH_DIR already exists, skipping..."
 fi
@@ -33,4 +35,9 @@ read -p "don't forget to install the Meslo font"
 if [ ! -d "$OHMYZSH_DIR/custom/themes/powerlevel10k" ]; then
 	git clone --depth=1 https://github.com/romkatv/powerlevel10k.git $CUSTOM_DIR/themes/powerlevel10k
 fi
+
+if [ ! -d "$HOME/.asdf" ]; then
+	git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.11.3
+fi
+
 
