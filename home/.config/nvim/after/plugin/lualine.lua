e1195155ae2fb0ed25b3795457486ce2916eb6ee
Javier Portillo <17260001+javierportillo@users.noreply.github.com>
2023-05-23 15:47:04 -0600

    feat(nvim): customize lualine

--- home/.config/nvim/after/plugin/lualine.lua
+++ home/.config/nvim/after/plugin/lualine.lua
@@ -1,1 +1,21 @@
-require("lualine").setup()
+require("lualine").setup({
+  options = {
+    theme = "tokyonight",
+    component_separators = {
+      left = "",
+      right = "",
+    },
+    section_separators = {
+      left = "",
+      right = "",
+    },
+  },
+  sections = {
+    lualine_b = {
+      {
+        "diff",
+        colored = false,
+      },
+    },
+  },
+})
