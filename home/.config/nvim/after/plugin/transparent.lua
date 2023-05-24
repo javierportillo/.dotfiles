1f9e53bd5c8c1b86f29abafb9a7008d5e225057b
Javier Portillo <17260001+javierportillo@users.noreply.github.com>
2023-05-24 10:49:20 -0600

    feat(nvim): add transparency for telescope float

--- home/.config/nvim/after/plugin/transparent.lua
+++ home/.config/nvim/after/plugin/transparent.lua
@@ -5,6 +5,13 @@ require("transparent").setup({
     'Conditional', 'Repeat', 'Operator', 'Structure', 'LineNr', 'NonText',
     'SignColumn', 'CursorLineNr', 'EndOfBuffer',
   },
-  extra_groups = {}, -- table: additional groups that should be cleared
+  extra_groups = {
+    'NormalFloat',
+    'TelescopeNormal',
+    'TelescopePromptNormal',
+    'TelescopePromptBorder',
+    'TelescopeResultsBorder',
+    'TelescopePreviewBorder',
+  }, -- table: additional groups that should be cleared
   exclude_groups = {}, -- table: groups you don't want to clear
 })
