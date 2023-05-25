8ae6dcaef9a3cd405d4d4771a469fa3bfd85fffb
Javier Portillo <17260001+javierportillo@users.noreply.github.com>
2023-05-25 13:16:26 -0600

    feat(nvim): change telescope find_grep default arguments

--- home/.config/nvim/after/plugin/telescope.lua
+++ home/.config/nvim/after/plugin/telescope.lua
@@ -6,11 +6,24 @@ vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
 vim.keymap.set('n', '<leader>fp', builtin.git_files, {})
 vim.keymap.set('n', '<leader>fc', builtin.commands, {})
 vim.keymap.set('n', '<leader>fm', builtin.man_pages, {})
-vim.keymap.set('n', '<leader>fth', function () builtin.colorscheme({ enable_preview = true }) end, {})
+vim.keymap.set('n', '<leader>fth', function() builtin.colorscheme({ enable_preview = true }) end, {})
 
 -- You dont need to set any of these options. These are the default ones. Only
 -- the loading is important
 require('telescope').setup {
+  defaults = {
+    vimgrep_arguments = {
+      'rg',
+      '-.', -- search dotfiles
+      '--color=never',
+      '--no-heading',
+      '--with-filename',
+      '--line-number',
+      '--column',
+      '--smart-case',
+    },
+    file_ignore_patterns = { "node_modules", ".git" },
+  },
   pickers = {
     find_files = {
       find_command = { "rg", "--files", "--hidden", "--glob", "!**/.git/*" },
@@ -18,15 +31,14 @@ require('telescope').setup {
   },
   extensions = {
     fzf = {
-      fuzzy = true,                    -- false will only do exact matching
-      override_generic_sorter = true,  -- override the generic sorter
-      override_file_sorter = true,     -- override the file sorter
-      case_mode = "smart_case",        -- or "ignore_case" or "respect_case"
-                                       -- the default case_mode is "smart_case"
+      fuzzy = true,                   -- false will only do exact matching
+      override_generic_sorter = true, -- override the generic sorter
+      override_file_sorter = true,    -- override the file sorter
+      case_mode = "smart_case",       -- or "ignore_case" or "respect_case"
+      -- the default case_mode is "smart_case"
     }
   }
 }
 -- To get fzf loaded and working with telescope, you need to call
 -- load_extension, somewhere after setup function:
 require('telescope').load_extension('fzf')
-
