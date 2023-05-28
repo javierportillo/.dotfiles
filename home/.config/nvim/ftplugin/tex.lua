5664ea6ac153a82360897da2cec2cff4c8084194
Javier Portillo <17260001+javierportillo@users.noreply.github.com>
2023-05-28 00:40:35 -0600

    feat(nvim): add ftplugin for tex files
    add keymaps
    - <leader>tl to compile and lauch sumatra
    - <leader>tt to just compile

---
+++ home/.config/nvim/ftplugin/tex.lua
@@ -0,0 +1,30 @@
+local function compileToPDF()
+  local current_buffer = vim.fn.expand("%")
+  local command = "pdflatex " .. current_buffer
+  vim.fn.jobstart(command, {
+    on_exit = function(_, code)
+      if code ~= 0 then
+        print("pdflatex could not compile this file: " .. code)
+      end
+    end
+  })
+end
+
+vim.keymap.set("n", "<leader>tl", function()
+  compileToPDF()
+  local current_buffer = vim.fn.expand("%")
+  local file_extension = vim.fn.expand("%:e")
+  local pdf_file = current_buffer:gsub(file_extension .. "$", "pdf")
+  -- yup, sadly, im running on windows using WSL, no other choise for now...
+  local command = "SumatraPDF.exe -reuse-instance " .. pdf_file
+  vim.fn.jobstart(command, {
+    on_exit = function(_, code)
+      if code ~= 0 then
+        print("SumatraPDF could not open this file: " .. code)
+      end
+    end
+  })
+end, { buffer = true  })
+
+
+vim.keymap.set("n", "<leader>tt", compileToPDF, { buffer = true  })
