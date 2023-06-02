d1409f3437b82b7f602eb9892eacf20b829d81e2
Javier Portillo <17260001+javierportillo@users.noreply.github.com>
2023-06-02 16:04:56 -0600

    feat(nvim): change icons for git

--- home/.config/nvim/after/plugin/neo-tree.lua
+++ home/.config/nvim/after/plugin/neo-tree.lua
@@ -2,11 +2,11 @@
 vim.cmd([[ let g:neo_tree_remove_legacy_commands = 1 ]])
 
 vim.fn.sign_define("DiagnosticSignError",
-  { text = "", texthl = "DiagnosticSignError" })
+  { text = "x", texthl = "DiagnosticSignError" })
 vim.fn.sign_define("DiagnosticSignWarn",
-  { text = "", texthl = "DiagnosticSignWarn" })
+  { text = "!", texthl = "DiagnosticSignWarn" })
 vim.fn.sign_define("DiagnosticSignInfo",
-  { text = "", texthl = "DiagnosticSignInfo" })
+  { text = "i", texthl = "DiagnosticSignInfo" })
 vim.fn.sign_define("DiagnosticSignHint",
   { text = "", texthl = "DiagnosticSignHint" })
 
