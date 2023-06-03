0117a08c36e2313437c0918ba534c83a5bdca3ff
Javier Portillo <17260001+javierportillo@users.noreply.github.com>
2023-06-02 22:06:58 -0600

    feat(nvim): add config for project ".dotfiles"

---
+++ home/.config/nvim/projects/.dotfiles.lua
@@ -0,0 +1,18 @@
+vim.api.nvim_create_user_command("SyncAlacritty", function()
+  local command = "cd $HOME && rsync .dotfiles/home/.config/alacritty/alacritty.yml winhome/AppData/Roaming/alacritty"
+  vim.fn.jobstart(command, {
+    on_exit = function(_, code)
+      if code ~= 0 then
+        print("could not sync to windows")
+      else
+        print("synced to windows")
+      end
+    end
+  })
+end, {})
+
+vim.api.nvim_create_autocmd("BufWritePost", {
+  group = vim.api.nvim_create_augroup("AutoSyncAlacritty", { clear = true }),
+  pattern = "alacritty.yml",
+  command = "SyncAlacritty",
+})
