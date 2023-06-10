f9bc075ddecb39681dc5b9b446aaceea2d28c0f2
Javier Portillo <17260001+javierportillo@users.noreply.github.com>
2023-06-09 20:40:53 -0600

    feat(nvim): lsp remove group index for cmp

--- home/.config/nvim/after/plugin/lsp.lua
+++ home/.config/nvim/after/plugin/lsp.lua
@@ -102,11 +102,11 @@ cmp.setup({
     }),
   },
   sources = {
-    { name = "nvim_lsp", group_index = 1 },
+    { name = "nvim_lsp" },
     { name = "nvim_lua" },
     { name = "luasnip" },
     { name = "path" },
-    { name = "buffer",   keyword_length = 5, group_index = 2 },
+    { name = "buffer",   keyword_length = 5 },
   },
   view = {
     entries = {
