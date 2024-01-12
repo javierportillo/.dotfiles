7e5516268069294c9efeba10371780e483ab6a69
Javier Portillo <17260001+javierportillo@users.noreply.github.com>
2024-01-12 13:53:14 -0600

    adds wezterm basic config and sync to windows

--- home/.config/nvim/projects/.dotfiles.lua
+++ home/.config/nvim/projects/.dotfiles.lua
@@ -3,9 +3,9 @@ vim.api.nvim_create_user_command("SyncAlacritty", function()
   vim.fn.jobstart(command, {
     on_exit = function(_, code)
       if code ~= 0 then
-        print("could not sync to windows")
+        print("could not sync alacritty to windows")
       else
-        print("synced to windows")
+        print("synced alacritty to windows")
       end
     end
   })
@@ -15,4 +15,24 @@ vim.api.nvim_create_autocmd("BufWritePost", {
   group = vim.api.nvim_create_augroup("AutoSyncAlacritty", { clear = true }),
   pattern = "alacritty.yml",
   command = "SyncAlacritty",
+})
+
+
+vim.api.nvim_create_user_command("SyncWezterm", function()
+  local command = "cd $HOME && rsync .dotfiles/home/.config/wezterm/.wezterm.lua winhome/"
+  vim.fn.jobstart(command, {
+    on_exit = function(_, code)
+      if code ~= 0 then
+        print("could not sync wezterm to windows")
+      else
+        print("synced wezterm to windows")
+      end
+    end
+  })
+end, {})
+
+vim.api.nvim_create_autocmd("BufWritePost", {
+  group = vim.api.nvim_create_augroup("AutoSyncWezterm", { clear = true }),
+  pattern = ".wezterm.lua",
+  command = "SyncWezterm",
 })
