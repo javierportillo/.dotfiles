af96048ae20f3b2e183a29cc3134e28ce776c0a5
Javier Portillo <17260001+javierportillo@users.noreply.github.com>
2024-01-12 22:47:19 -0600

    hide .git dir from neo-tree

--- home/.config/nvim/lua/plugins/neo-tree.lua
+++ home/.config/nvim/lua/plugins/neo-tree.lua
@@ -173,6 +173,7 @@ return {
             hide_hidden = false, -- only works on Windows for hidden files/directories
             hide_by_name = {
               --"node_modules"
+              ".git"
             },
             hide_by_pattern = { -- uses glob style patterns
               --"*.meta",
