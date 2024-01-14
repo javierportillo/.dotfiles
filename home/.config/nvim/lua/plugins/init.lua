2698eb44c8e863b7ae25bdf88a6415d0c920ab34
Javier Portillo <17260001+javierportillo@users.noreply.github.com>
2024-01-13 23:00:54 -0600

    moves lsp config

--- home/.config/nvim/lua/plugins/init.lua
+++ home/.config/nvim/lua/plugins/init.lua
@@ -1,36 +1,7 @@
 return {
-  {
-    'VonHeikemen/lsp-zero.nvim',
-    branch = 'v2.x',
-    dependencies = {
-      { 'neovim/nvim-lspconfig' },
-      {
-        'williamboman/mason.nvim',
-      },
-      { 'williamboman/mason-lspconfig.nvim' },
-      { 'onsails/lspkind.nvim' },
-      { 'hrsh7th/nvim-cmp' },
-      { 'hrsh7th/cmp-nvim-lsp' },
-      { 'hrsh7th/cmp-nvim-lua' },
-      { 'hrsh7th/cmp-buffer' },
-      { 'hrsh7th/cmp-path' },
-      { 'hrsh7th/cmp-cmdline' },
-      { 'saadparwaiz1/cmp_luasnip' },
-      { 'L3MON4D3/LuaSnip' },
-      { 'rafamadriz/friendly-snippets' },
-    }
-  },
-
-  {
-    'jose-elias-alvarez/null-ls.nvim',
-    dependencies = { 'nvim-lua/plenary.nvim' },
-  },
-
   'christoomey/vim-tmux-navigator',
   'tpope/vim-obsession',
   'tpope/vim-surround',
-  'christoomey/vim-tmux-navigator',
   'cohama/lexima.vim',
-  'b0o/schemastore.nvim',
 
 }
--- home/.config/nvim/lua/plugins/lsp.lua
+++ home/.config/nvim/lua/plugins/lsp.lua
@@ -1,3 +1,29 @@
+return {
+  {
+    'VonHeikemen/lsp-zero.nvim',
+    branch = 'v2.x',
+    dependencies = {
+      'neovim/nvim-lspconfig',
+      'williamboman/mason.nvim',
+      'williamboman/mason-lspconfig.nvim',
+      'onsails/lspkind.nvim',
+      'hrsh7th/nvim-cmp',
+      'hrsh7th/cmp-nvim-lsp',
+      'hrsh7th/cmp-nvim-lua',
+      'hrsh7th/cmp-buffer',
+      'hrsh7th/cmp-path',
+      'hrsh7th/cmp-cmdline',
+      'saadparwaiz1/cmp_luasnip',
+      'L3MON4D3/LuaSnip',
+      'rafamadriz/friendly-snippets',
+      'b0o/schemastore.nvim',
+      {
+        'jose-elias-alvarez/null-ls.nvim',
+        dependencies = { 'nvim-lua/plenary.nvim' },
+      },
+    },
+    config = function ()
+
 local lsp = require("lsp-zero").preset({
   name = 'minimal',
   float_border = false,
@@ -151,3 +177,6 @@ null_ls.setup({
     null_ls.builtins.formatting.bibclean,
   }
 })
+    end
+  },
+}
