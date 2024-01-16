c799e2c79aff92418e7a7bd76c60a707006417c1
Javier Portillo <17260001+javierportillo@users.noreply.github.com>
2024-01-16 11:42:33 -0600

    disables some vim-unimpaired keymaps

--- home/.config/nvim/lua/plugins/init.lua
+++ home/.config/nvim/lua/plugins/init.lua
@@ -2,6 +2,5 @@ return {
   'christoomey/vim-tmux-navigator',
   'tpope/vim-obsession',
   'tpope/vim-surround',
-  'tpope/vim-unimpaired',
   'cohama/lexima.vim',
 }
---
+++ home/.config/nvim/lua/plugins/unimpaired.lua
@@ -0,0 +1,20 @@
+return {
+  {
+    'tpope/vim-unimpaired',
+    init = function()
+      vim.g["nremap"] = {
+        -- disable xml encode keymaps, need them for folke/trouble.nvim
+        ["[x"] = "",
+        ["[xx"] = "",
+        ["]x"] = "",
+        ["]xx"] = "",
+      }
+      vim.g["xremap"] = {
+        ["[x"] = "",
+        ["[xx"] = "",
+        ["]x"] = "",
+        ["]xx"] = "",
+      }
+    end
+  }
+}
