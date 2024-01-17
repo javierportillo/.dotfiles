6f3b38ab943d3fb5acd12ec93f2d7ff1cdcf82d5
Javier Portillo <17260001+javierportillo@users.noreply.github.com>
2024-01-17 15:06:56 -0600

    updates to rainbow-delimiters.nvim

--- home/.config/nvim/lua/plugins/treesitter.lua
+++ home/.config/nvim/lua/plugins/treesitter.lua
@@ -2,7 +2,6 @@ return {
   {
     'nvim-treesitter/nvim-treesitter',
     dependencies = {
-      'HiPhish/nvim-ts-rainbow2',
       'nvim-treesitter/nvim-treesitter-textobjects',
       'windwp/nvim-ts-autotag',
     },
@@ -36,15 +35,6 @@ return {
           -- Using this option may slow down your editor, and you may see some duplicate highlights.
           -- Instead of true it can also be a list of languages
           additional_vim_regex_highlighting = false,
-        },
-        rainbow = {
-          enable = true,
-          -- list of languages you want to disable the plugin for
-          -- disable = { 'jsx', 'cpp' },
-          -- Which query to use for finding delimiters
-          query = 'rainbow-parens',
-          -- Highlight the entire buffer all at once
-          strategy = require('ts-rainbow').strategy.global,
         },
         incremental_selection = {
           enable = true,
@@ -108,5 +98,8 @@ return {
   },
   {
     'nvim-treesitter/nvim-treesitter-context',
-  }
+  },
+  {
+    'HiPhish/rainbow-delimiters.nvim',
+  },
 }
