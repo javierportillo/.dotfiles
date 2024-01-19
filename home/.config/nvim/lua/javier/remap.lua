918f8095a14b956b26c74396bb1e8f92a31a07e7
Javier Portillo <17260001+javierportillo@users.noreply.github.com>
2024-01-19 14:05:52 -0600

    Adds quality of life key maps

--- home/.config/nvim/lua/javier/remap.lua
+++ home/.config/nvim/lua/javier/remap.lua
@@ -6,13 +6,21 @@ vim.keymap.set("n", "n", "nzzzv")
 vim.keymap.set("n", "N", "Nzzzv")
 
 -- next greatest remap ever : asbjornHaland
-vim.keymap.set({"n", "v"}, "<leader>y", [["+y]])
+vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]])
 vim.keymap.set("n", "<leader>Y", [["+Y]])
 
-vim.keymap.set({"n", "v"}, "<leader>d", [["_d]])
+vim.keymap.set({ "n", "v" }, "<leader>d", [["_d]])
 
--- This is going to get me cancelled
-vim.keymap.set("i", "<C-c>", "<Esc>")
+-- "This is going to get me cancelled" - The Primagen
+-- Don't need it with my kb at home (swapped caps to esc)
+-- it actually makes it worse when some plugin is expecting <C-c> and gets <Esc>
+-- vim.keymap.set("i", "<C-c>", "<Esc>")
 
 vim.keymap.set("n", "Q", "<nop>")
 
+-- Swap ; for : in visual and normal
+vim.keymap.set({ "n", "v" }, ";", ":")
+vim.keymap.set({ "n", "v" }, ":", ";")
+
+-- Paste last thing yanked, not deleted
+vim.keymap.set({ "n", "v" }, ",p", [["0p]])
