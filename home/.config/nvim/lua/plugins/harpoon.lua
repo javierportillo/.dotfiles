aff8b92876426a0f8944451ba2a75fb865e32e5e
Javier Portillo <17260001+javierportillo@users.noreply.github.com>
2024-01-30 12:13:50 -0600

    change harpoon keymap to leader as alt is taken over by komorebi

--- home/.config/nvim/lua/plugins/harpoon.lua
+++ home/.config/nvim/lua/plugins/harpoon.lua
@@ -8,10 +8,11 @@ return {
       vim.keymap.set("n", "<leader>a", mark.add_file)
       vim.keymap.set("n", "<C-e>", ui.toggle_quick_menu)
 
-      vim.keymap.set("n", "<M-h>", function() ui.nav_file(1) end)
-      vim.keymap.set("n", "<M-j>", function() ui.nav_file(2) end)
-      vim.keymap.set("n", "<M-k>", function() ui.nav_file(3) end)
-      vim.keymap.set("n", "<M-l>", function() ui.nav_file(4) end)
+      vim.keymap.set("n", "<leader>1", function() ui.nav_file(1) end)
+      vim.keymap.set("n", "<leader>2", function() ui.nav_file(2) end)
+      vim.keymap.set("n", "<leader>3", function() ui.nav_file(3) end)
+      vim.keymap.set("n", "<leader>4", function() ui.nav_file(4) end)
+      vim.keymap.set("n", "<leader>5", function() ui.nav_file(5) end)
     end
   }
 
