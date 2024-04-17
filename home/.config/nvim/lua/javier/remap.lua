419b4222f93d6d8030604fb587d4b55d21524624
Javier Portillo <17260001+javierportillo@users.noreply.github.com>
2024-04-16 19:16:00 -0600

    remove move selected lines

--- home/.config/nvim/lua/javier/remap.lua
+++ home/.config/nvim/lua/javier/remap.lua
@@ -1,5 +1,5 @@
-vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
-vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+-- vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+-- vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
 vim.keymap.set("n", "J", "mzJ`z")
 vim.keymap.set("n", "n", "nzzzv")
