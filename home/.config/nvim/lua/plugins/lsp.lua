e1b2ac5ea5424080268a07c9ca4690562342b62a
Javier Portillo <17260001+javierportillo@users.noreply.github.com>
2024-08-20 22:26:13 -0600

    improves diagnostic information

--- home/.config/nvim/lua/plugins/lsp.lua
+++ home/.config/nvim/lua/plugins/lsp.lua
@@ -75,6 +75,9 @@ return {
           --   For example, in C this would take you to the header.
           map('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
 
+          -- Open Diagnostics floating window
+          map('<leader>D', vim.diagnostic.open_float, 'Open [D]iagnostic Window')
+
           -- The following two autocommands are used to highlight references of the
           -- word under your cursor when your cursor rests there for a little while.
           --   See `:help CursorHold` for information about when this is executed.
@@ -189,6 +192,13 @@ return {
             server.capabilities = vim.tbl_deep_extend('force', {}, capabilities, server.capabilities or {})
             require('lspconfig')[server_name].setup(server)
           end,
+        },
+      })
+
+      -- Diagnostics configuration
+      vim.diagnostic.config({
+        float = {
+          source = 'if_many',
         },
       })
     end,
