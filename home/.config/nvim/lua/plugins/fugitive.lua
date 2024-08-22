a92e24a200072671527a3dd94b385cc1c3f447e5
Javier Portillo <17260001+javierportillo@users.noreply.github.com>
2024-08-22 15:43:18 -0600

    Adds event to lazy load vim-fugitive
    So the :G commands are available from the start

--- home/.config/nvim/lua/plugins/fugitive.lua
+++ home/.config/nvim/lua/plugins/fugitive.lua
@@ -1,6 +1,7 @@
 return {
   {
     'tpope/vim-fugitive',
+    event = 'vimEnter',
     keys = {
       { '<leader>gs', vim.cmd.Git, desc = 'Open Git Status' },
       { '<leader>gl', '<cmd>G log --oneline --decorate --graph --all<cr>', desc = 'Open Git Log' },
