7d613f6fefb89dc61abaea0b9ee4bf50fee757fe
Javier Portillo <17260001+javierportillo@users.noreply.github.com>
2024-08-23 15:03:03 -0600

    Fix lazy event name for vim-fugitive plugin

--- home/.config/nvim/lua/plugins/git.lua
+++ home/.config/nvim/lua/plugins/git.lua
@@ -1,7 +1,7 @@
 return {
   {
     'tpope/vim-fugitive',
-    event = 'vimEnter',
+    event = 'VimEnter',
     keys = {
       { '<leader>gs', vim.cmd.Git, desc = 'Open Git Status' },
       { '<leader>gl', '<cmd>G log --oneline --decorate --graph --all<cr>', desc = 'Open Git Log' },
