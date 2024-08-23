753fa2f2e9c3b2339d9cb9277e42a745170806db
Javier Portillo <17260001+javierportillo@users.noreply.github.com>
2024-08-23 13:31:33 -0600

    Moves git plugins to single file

--- home/.config/nvim/lua/plugins/git.lua
+++ home/.config/nvim/lua/plugins/git.lua
@@ -1,5 +1,13 @@
 return {
   {
+    'tpope/vim-fugitive',
+    event = 'vimEnter',
+    keys = {
+      { '<leader>gs', vim.cmd.Git, desc = 'Open Git Status' },
+      { '<leader>gl', '<cmd>G log --oneline --decorate --graph --all<cr>', desc = 'Open Git Log' },
+    },
+  },
+  {
     -- best Git client ever
     'kdheepak/lazygit.nvim',
     -- optional for floating window border decoration
@@ -8,6 +16,6 @@ return {
     },
     keys = {
       { '<leader>gg', vim.cmd.LazyGit, desc = 'Open LazyGit' },
-    }
+    },
   },
 }
