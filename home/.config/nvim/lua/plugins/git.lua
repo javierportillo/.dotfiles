3590703b9e9c01fa8a35e920bf534a8f21f8312a
Javier Portillo <17260001+javierportillo@users.noreply.github.com>
2024-08-25 11:38:36 -0600

    Removes git log keybind from fugitive
    I will be handling it with DiffView from now on

--- home/.config/nvim/lua/plugins/git.lua
+++ home/.config/nvim/lua/plugins/git.lua
@@ -4,7 +4,6 @@ return {
     event = 'VimEnter',
     keys = {
       { '<leader>gs', vim.cmd.Git, desc = 'Open Git Status' },
-      { '<leader>gl', '<cmd>G log --oneline --decorate --graph --all<cr>', desc = 'Open Git Log' },
     },
   },
   {
