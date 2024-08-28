1c37eb493886889e12a5034aaa01538828ca9eb3
Javier Portillo <17260001+javierportillo@users.noreply.github.com>
2024-08-28 01:11:55 -0600

    Adds lazy config for development packages

--- home/.config/nvim/lua/javier/plugins.lua
+++ home/.config/nvim/lua/javier/plugins.lua
@@ -1,12 +1,12 @@
 -- [[ Install `lazy.nvim` plugin manager ]]
 --    See `:help lazy.nvim.txt` or https://github.com/folke/lazy.nvim for more info
 
-local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
+local lazypath = vim.fn.stdpath('data') .. '/lazy/lazy.nvim'
 
 ---@diagnostic disable-next-line: undefined-field
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
   local lazyrepo = 'https://github.com/folke/lazy.nvim.git'
-  local out = vim.fn.system { 'git', 'clone', '--filter=blob:none', '--branch=stable', lazyrepo, lazypath }
+  local out = vim.fn.system({ 'git', 'clone', '--filter=blob:none', '--branch=stable', lazyrepo, lazypath })
   if vim.v.shell_error ~= 0 then
     error('Error cloning lazy.nvim:\n' .. out)
   end
@@ -20,6 +20,9 @@ require('lazy').setup({
   change_detection = {
     notify = false,
   },
+  ---@diagnostic disable-next-line: assign-type-mismatch
+  dev = {
+    path = '~/Documents/nvim_dev_projects',
+    fallback = false,
+  },
 })
-
-
