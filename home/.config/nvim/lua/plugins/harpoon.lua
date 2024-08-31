c4ccf9ebe4a303ae8ea29e6fdc9e4141df7e2170
Javier Portillo <17260001+javierportillo@users.noreply.github.com>
2024-08-31 11:26:33 -0600

    Updates to harpoon2

--- home/.config/nvim/lua/plugins/harpoon.lua
+++ home/.config/nvim/lua/plugins/harpoon.lua
@@ -1,20 +1,36 @@
 return {
   {
-    'theprimeagen/harpoon',
+    'ThePrimeagen/harpoon',
+    branch = 'harpoon2',
     event = 'VimEnter',
+    dependencies = { 'nvim-lua/plenary.nvim' },
     config = function()
-      local mark = require('harpoon.mark')
-      local ui = require('harpoon.ui')
+      local harpoon = require('harpoon')
 
-      vim.keymap.set('n', '<leader>a', mark.add_file, { desc = 'Add to Harpoon' })
-      vim.keymap.set('n', '<C-e>', ui.toggle_quick_menu, { desc = 'Open Harpoon Dialog' })
+      harpoon:setup()
 
-      vim.keymap.set('n', '<leader>1', function() ui.nav_file(1) end, { desc = 'Harpoon to 1' })
-      vim.keymap.set('n', '<leader>2', function() ui.nav_file(2) end, { desc = 'Harpoon to 2' })
-      vim.keymap.set('n', '<leader>3', function() ui.nav_file(3) end, { desc = 'Harpoon to 3' })
-      vim.keymap.set('n', '<leader>4', function() ui.nav_file(4) end, { desc = 'Harpoon to 4' })
-      vim.keymap.set('n', '<leader>5', function() ui.nav_file(5) end, { desc = 'Harpoon to 5' })
-    end
-  }
+      vim.keymap.set('n', '<leader>a', function()
+        harpoon:list():add()
+      end)
+      vim.keymap.set('n', '<C-e>', function()
+        harpoon.ui:toggle_quick_menu(harpoon:list())
+      end)
 
+      vim.keymap.set('n', '<leader>1', function()
+        harpoon:list():select(1)
+      end, { desc = 'Harpoon to 1' })
+      vim.keymap.set('n', '<leader>2', function()
+        harpoon:list():select(2)
+      end, { desc = 'Harpoon to 2' })
+      vim.keymap.set('n', '<leader>3', function()
+        harpoon:list():select(3)
+      end, { desc = 'Harpoon to 3' })
+      vim.keymap.set('n', '<leader>4', function()
+        harpoon:list():select(4)
+      end, { desc = 'Harpoon to 4' })
+      vim.keymap.set('n', '<leader>5', function()
+        harpoon:list():select(5)
+      end, { desc = 'Harpoon to 5' })
+    end,
+  },
 }
