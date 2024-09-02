5a4bda5f747bbe52686960ec565d5f6a28b30c1a
Javier Portillo <17260001+javierportillo@users.noreply.github.com>
2024-09-02 12:18:09 -0600

    Adds hydra head to resize splits

--- home/.config/nvim/lua/plugins/hydra.lua
+++ home/.config/nvim/lua/plugins/hydra.lua
@@ -10,15 +10,29 @@ return {
         mode = 'n',
         body = 'z',
         config = {
-          hint = {
-            type = 'statusline',
-          },
+          -- hint = { type = 'window' },
+          hint = false,
         },
         heads = {
           { 'h', '5zh' },
           { 'l', '5zl', { desc = '←/→' } },
           { 'H', 'zH' },
           { 'L', 'zL', { desc = 'half screen ←/→' } },
+        },
+      })
+
+      Hydra({
+        name = 'Split resize',
+        mode = 'n',
+        body = '<c-w>',
+        config = {
+          hint = false,
+        },
+        heads = {
+          { '+', '<c-w>5+' },
+          { '-', '<c-w>5-', { desc = '+/- height' } },
+          { '<', '<c-w>5<' },
+          { '>', '<c-w>5>', { desc = '</> width' } },
         },
       })
     end,
