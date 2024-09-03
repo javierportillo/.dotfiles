e357ed5d5ee610286648958fb546fca9929b4bbd
Javier Portillo <17260001+javierportillo@users.noreply.github.com>
2024-09-03 13:54:33 -0600

    Makes Notify highlight groups be transparent

--- home/.config/nvim/lua/plugins/transparent.lua
+++ home/.config/nvim/lua/plugins/transparent.lua
@@ -3,6 +3,14 @@ return {
     'xiyaowong/transparent.nvim',
     lazy = false,
     opts = {
+      extra_groups = {
+        'Normal',
+        'NotifyERRORBody',
+        'NotifyWARNBody',
+        'NotifyINFOBody',
+        'NotifyDEBUGBody',
+        'NotifyTRACEBody',
+      },
       exclude_groups = {
         'CursorLine',
       },
