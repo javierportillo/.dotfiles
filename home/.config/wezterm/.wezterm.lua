8caad5bb6e62d1cf0bbe68e7e6ec56afb5105108
Javier Portillo <17260001+javierportillo@users.noreply.github.com>
2024-09-03 13:47:28 -0600

    Toggle wezterm full screen on zen mode

--- home/.config/wezterm/.wezterm.lua
+++ home/.config/wezterm/.wezterm.lua
@@ -149,19 +149,32 @@ wezterm.on('user-var-changed', function(window, pane, name, value)
   if name == 'ZEN_MODE' then
     local incremental = value:find('+')
     local number_value = tonumber(value)
+    local dimensions = window:get_dimensions()
+    local should_full_screen = false
+
     if incremental ~= nil then
       while number_value > 0 do
         window:perform_action(wezterm.action.IncreaseFontSize, pane)
         number_value = number_value - 1
       end
       overrides.enable_tab_bar = false
+      should_full_screen = true
     elseif number_value < 0 then
       window:perform_action(wezterm.action.ResetFontSize, pane)
       overrides.font_size = nil
       overrides.enable_tab_bar = true
+      should_full_screen = false
     else
       overrides.font_size = number_value
       overrides.enable_tab_bar = false
+      should_full_screen = true
+    end
+
+    if dimensions.is_full_screen and not should_full_screen then
+      window:toggle_fullscreen()
+    end
+    if not dimensions.is_full_screen and should_full_screen then
+      window:toggle_fullscreen()
     end
   end
   window:set_config_overrides(overrides)
