ace78594b3539a0cf39fb5f14014c628428eb9a7
Javier Portillo <17260001+javierportillo@users.noreply.github.com>
2024-09-05 22:39:36 -0600

    Adds mini.ai plugin

--- home/.config/nvim/lua/plugins/mini.lua
+++ home/.config/nvim/lua/plugins/mini.lua
@@ -5,6 +5,26 @@ return {
     event = 'VeryLazy',
     config = function()
       require('mini.align').setup()
+
+      local spec_treesitter = require('mini.ai').gen_spec.treesitter
+      require('mini.ai').setup({
+        custom_textobjects = {
+          -- whole buffer
+          g = function()
+            local from = { line = 1, col = 1 }
+            local to = {
+              line = vim.fn.line('$'),
+              col = math.max(vim.fn.getline('$'):len(), 1),
+            }
+            return { from = from, to = to }
+          end,
+          F = spec_treesitter({ a = '@function.outer', i = '@function.inner' }),
+          o = spec_treesitter({
+            a = { '@conditional.outer', '@loop.outer' },
+            i = { '@conditional.inner', '@loop.inner' },
+          }),
+        },
+      })
     end,
   },
 }
