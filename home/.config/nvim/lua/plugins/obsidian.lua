12fa74be6c424081c4f4a2e07242bae6b27a1093
Javier Portillo <17260001+javierportillo@users.noreply.github.com>
2024-09-05 20:34:14 -0600

    Expands on the obsidian configuration

--- home/.config/nvim/lua/plugins/obsidian.lua
+++ home/.config/nvim/lua/plugins/obsidian.lua
@@ -25,24 +25,59 @@ return {
           path = '/mnt/c/Users/javie/Google Drive/obsidian/My Second Brain',
         },
       },
+
       daily_notes = {
         folder = 'daily',
         default_tags = { 'daily' },
+        template = 'daily.md',
       },
+
       completion = {
         nvim_cmp = true,
       },
-      wiki_link_func = 'use_alias_only',
+
+      -- Optional, customize how note IDs are generated given an optional title.
+      ---@param title string|?
+      ---@return string
+      note_id_func = function(title)
+        -- Create note IDs in a Zettelkasten format with a timestamp and a suffix.
+        -- In this case a note with the title 'My new note' will be given an ID that looks
+        -- like '1657296016-my-new-note', and therefore the file name '1657296016-my-new-note.md'
+        local suffix = ''
+        if title ~= nil then
+          -- If title is given, transform it into valid file name.
+          suffix = title:gsub(' ', '-'):gsub('[^A-Za-z0-9-]', ''):lower()
+        else
+          -- If title is nil, just add 4 random uppercase letters to the suffix.
+          for _ = 1, 4 do
+            suffix = suffix .. string.char(math.random(65, 90))
+          end
+        end
+        return tostring(os.time()) .. '-' .. suffix
+      end,
+
+      wiki_link_func = function(opts)
+        return require('obsidian.util').wiki_link_id_prefix(opts)
+      end,
+
       preferred_link_style = 'wiki',
+
+      templates = {
+        folder = 'templates',
+      },
+
       follow_url_func = function(url)
         vim.fn.jobstart({ 'xdg-open', url })
       end,
+
       follow_img_func = function(url)
         vim.fn.jobstart({ 'xdg-open', url })
       end,
+
       picker = {
         name = 'telescope.nvim',
       },
+
       attachments = {
         img_folder = 'pictures',
       },
